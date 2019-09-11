a8eb3c41c575ba7652a7c828f184f9a97e12627d
william.tarpley <william.tarpley@NEITD-211216>
2019-09-11 18:21:46 -0500

    initial push for section 2 of our C# course.

---
+++ classwork/MovieLibrary/Itse1430.MovieLib.Host/MainForm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Itse1430.MovieLib.Host
+{
+    public partial class MainForm : Form
+    {
+        public MainForm ()
+        {
+            //int x = 10;
+            InitializeComponent ();
+        }
+    }
+}
